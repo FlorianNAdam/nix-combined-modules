c99c398bf53204268a94f9d0d7ce32d2055a5631
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2024-12-22 16:37:14 +0100

    progress

---
+++ modules/b.nix
@@ -0,0 +1,13 @@
+{ lib, config, ... }:
+let
+  cfg = config.myModule;
+in
+{
+  options = {
+    myModule.enable = lib.mkEnableOption "Enable Module";
+  };
+
+  config = lib.mkIf cfg.enable {
+    #config contents
+  };
+}
