f30647bd409a26d3cd277ad0bff47fb346b278de
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2024-12-22 16:18:28 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Modules for managing system configurations.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.flakeModule = import ./flakeModule.nix;
+      systems = import inputs.systems;
+      perSystem =
+        { pkgs, lib, ... }:
+        let
+          eval = lib.evalModules {
+            modules = import ./modules/all-modules.nix;
+          };
+        in
+        {
+          packages.docs =
+            (pkgs.nixosOptionsDoc {
+              options = eval.options;
+            }).optionsAsciiDoc;
+        };
+    };
+}
---
+++ flakeModule.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  inputs,
+  flake-parts-lib,
+  ...
+}:
+let
+  inherit (lib)
+    mkOption
+    types
+    ;
+  inherit (flake-parts-lib)
+    mkSubmoduleOptions
+    ;
+in
+{
+  options = {
+    # compatibility layer for home-manager
+    flake.homeConfigurations = mkOption {
+      type = types.lazyAttrsOf types.raw;
+      default = { };
+    };
+
+    nix-config = mkOption {
+      type = types.submoduleWith {
+        modules = (import ./modules/all-modules.nix) ++ [
+          { _module.args.inputs = inputs; }
+        ];
+      };
+    };
+  };
+
+  config = {
+    flake = {
+      homeConfigurations = config.nix-config.homeConfigurations;
+      nixosConfigurations = config.nix-config.nixosConfigurations;
+    };
+  };
+}
