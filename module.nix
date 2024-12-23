26ea825c32f37ff41114485a09632453e9fc70c0
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2024-12-23 15:13:17 +0100

    progress

--- module.nix
+++ module.nix
@@ -9,25 +9,27 @@ let
   cfg = config.nix-config;
 in
 {
-  options.nix-config = {
-    description = "Custom configuration containing NixOS and Home options";
-    type = lib.types.attrsOf (
-      lib.types.submodule {
-        options = {
-          nixos = lib.mkOption {
-            type = lib.types.attrs;
-            description = "NixOS-specific configuration options";
+  options = {
+    nix-config = lib.mkOption {
+      description = "Custom configuration containing NixOS and Home options";
+      type = lib.types.attrsOf (
+        lib.types.submodule {
+          options = {
+            nixos = lib.mkOption {
+              type = lib.types.attrs;
+              description = "NixOS-specific configuration options";
+            };
+            home = lib.mkOption {
+              type = lib.types.attrs;
+              description = "Home Manager-specific configuration options";
+            };
           };
-          home = lib.mkOption {
-            type = lib.types.attrs;
-            description = "Home Manager-specific configuration options";
-          };
-        };
-      }
-    );
-    default = {
-      nixos = { };
-      home = { };
+        }
+      );
+      default = {
+        nixos = { };
+        home = { };
+      };
     };
   };
 
