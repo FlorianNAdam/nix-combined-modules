c0fc0c6f8b5a1549a4c0c382ee370d067c6396c8
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-02-27 22:11:32 +0100

    add specialArgs to host modules

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -19,93 +19,101 @@ let
       inherit description;
     };
 
-  hostType = types.submodule (
-    { name, config, ... }:
-    {
-      options = {
-        name = mkOption {
-          type = types.str;
-          default = "";
-          description = ''
-            The name of the host, as specified in the attribute set. Use this to
-            target per-host behavior. Generally you should not set this yourself;
-            it will be set automatically when you define the host.
-          '';
-        };
-        kind = mkOption {
-          type = types.enum [
-            "nixos"
-            "home-manager"
-          ];
-          default = "nixos";
-          description = lib.mdDoc ''
-            The type of host this is. Two options:
+  hostType = types.submoduleWith {
+    specialArgs = config.specialArgs // {
+      hosts = config.hosts;
+    };
 
-            * `nixos`: A NixOS system configuration. Generates NixOS with
-              home-manager installed.
-            * `home-manager`: A home-manager configuration. Generates only the
-              home-manager configuration for the host.
-          '';
-        };
-        system = mkOption {
-          type = types.str;
-          default = null;
-          description = lib.mdDoc ''
-            The system that this host runs on. This is used to initialize
-            `nixpkgs`.
-          '';
-        };
+    modules = [
+      (
+        { name, config, ... }:
+        {
+          options = {
+            name = mkOption {
+              type = types.str;
+              default = "";
+              description = ''
+                The name of the host, as specified in the attribute set. Use this to
+                target per-host behavior. Generally you should not set this yourself;
+                it will be set automatically when you define the host.
+              '';
+            };
+            kind = mkOption {
+              type = types.enum [
+                "nixos"
+                "home-manager"
+              ];
+              default = "nixos";
+              description = lib.mdDoc ''
+                The type of host this is. Two options:
 
-        nix-config = mkModuleOption ''
-          additional configurations for nix-config-modules.
+                * `nixos`: A NixOS system configuration. Generates NixOS with
+                  home-manager installed.
+                * `home-manager`: A home-manager configuration. Generates only the
+                  home-manager configuration for the host.
+              '';
+            };
+            system = mkOption {
+              type = types.str;
+              default = null;
+              description = lib.mdDoc ''
+                The system that this host runs on. This is used to initialize
+                `nixpkgs`.
+              '';
+            };
 
-          Use this to add additional custom apps or customize apps
-          on a per-host basis.
-        '';
+            nix-config = mkModuleOption ''
+              additional configurations for nix-config-modules.
 
-        nixpkgs = mkModuleOption "nixpkgs configurations";
-        nixos = mkModuleOption "NixOS configurations";
-        home = mkModuleOption "home-manager configurations";
+              Use this to add additional custom apps or customize apps
+              on a per-host basis.
+            '';
 
-        username = mkOption {
-          type = types.str;
-          default = "user";
-          description = ''
-            The username of the single user for this system.
-          '';
-        };
-        email = mkOption {
-          type = types.str;
-          default = "";
-          description = ''
-            The email for the single user.
-          '';
-        };
-        homeDirectory = mkOption {
-          type = types.path;
-          default = "/home/${config.username}";
-          description = lib.mdDoc ''
-            The path to the home directory for this user. Defaults to
-            `/home/<username>`
-          '';
-        };
+            nixpkgs = mkModuleOption "nixpkgs configurations";
+            nixos = mkModuleOption "NixOS configurations";
+            home = mkModuleOption "home-manager configurations";
 
-        modules = mkOption {
-          type = types.listOf types.deferredModule;
-          default = [ ];
-        };
+            username = mkOption {
+              type = types.str;
+              default = "user";
+              description = ''
+                The username of the single user for this system.
+              '';
+            };
+            email = mkOption {
+              type = types.str;
+              default = "";
+              description = ''
+                The email for the single user.
+              '';
+            };
+            homeDirectory = mkOption {
+              type = types.path;
+              default = "/home/${config.username}";
+              description = lib.mdDoc ''
+                The path to the home directory for this user. Defaults to
+                `/home/<username>`
+              '';
+            };
 
-        stateVersion = mkOption {
-          type = types.str;
-        };
+            modules = mkOption {
+              type = types.listOf types.deferredModule;
+              default = [ ];
+            };
 
-      };
+            stateVersion = mkOption {
+              type = types.str;
+            };
 
-      config = {
-        inherit name;
-      };
-    }
-  );
+          };
+
+          config = {
+            inherit name;
+          };
+        }
+      )
+    ];
+  };
 in
 {
   options = {
